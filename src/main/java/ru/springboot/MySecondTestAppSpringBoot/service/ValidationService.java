0c4d3ad0b2a0bc772255f8da0c4ff30b7e3530ab
Dmitry Slobodchikov <dmitryslo@yandex.ru>
2024-09-29 22:36:30 +0500

    Initial commit

---
+++ src/main/java/ru/springboot/MySecondTestAppSpringBoot/service/ValidationService.java
@@ -0,0 +1,10 @@
+package ru.springboot.MySecondTestAppSpringBoot.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import ru.springboot.MySecondTestAppSpringBoot.exception.ValidationFailedException;
+
+@Service
+public interface ValidationService {
+    void isValid(BindingResult bindingResult) throws ValidationFailedException;
+}
