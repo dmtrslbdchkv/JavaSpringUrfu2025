a615f78c065cbc9cca889a26ca255e9b55f2f518
Dmitry Slobodchikov <dmitryslo@yandex.ru>
2024-10-19 09:44:07 +0500

    Fix LR1

--- src/main/java/ru/springboot/MySecondTestAppSpringBoot/HelloController/HelloController.java
+++ src/main/java/ru/springboot/MySecondTestAppSpringBoot/HelloController/HelloController.java
@@ -1,4 +1,4 @@
-package ru.slobodchikov.MyFirstTestAppSpringBoot.hello;
+package ru.springboot.MySecondTestAppSpringBoot.HelloController;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
